75030efbaa24b6a14c24e47ff32f4218bb37c043
Yaroslav Mihalev <j.mihalev1018@yandex.ru>
2025-07-18 00:10:47 +0300

    feat: Добавли live reload

--- server/index.js
+++ server/index.js
@@ -1,14 +1,12 @@
-// подключение express
-const express = require("express");
-
-// подключение path
-const path = require('path')
+import express from 'express';
+import reload from "reload"
+import {execSync} from "child_process";
 
 // создаем объект приложения
 const app = express();
 
 // указываем путь до папки со статикой
-app.use(express.static(path.join(__dirname, '../public')))
+app.use(express.static('public'))
 
 // определяем обработчик для маршрута "/"
 app.get("/", function(request, response){
@@ -17,5 +15,10 @@ app.get("/", function(request, response){
 });
 
 // начинаем прослушивать подключения на 3000 порту
-app.listen(3000);
-console.log("Server listening 3000");
+app.listen(3000, () => {
+    console.log("Server listening 3000");
+});
+
+reload(app).then(function () {
+    execSync("npm run build", { stdio: 'inherit', shell: true });
+});
