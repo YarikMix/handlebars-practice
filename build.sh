66a4b595fc09869863e6ae0018a553de61d1d588
Yaroslav Mihalev <j.mihalev1018@yandex.ru>
2025-07-27 17:42:31 +0300

    feat: Рефакторинг

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir -p public/build
-handlebars -m public/src/components/home.hbs -f public/build/home.js
-handlebars -m public/src/components/posts.hbs -f public/build/posts.js
-handlebars -m public/src/components/settings.hbs -f public/build/settings.js
+handlebars -m public/src/components/header/header.hbs -f public/build/header.js
+handlebars -m public/src/components/posts/posts.hbs -f public/build/posts.js
+handlebars -m public/src/components/post/post.hbs -f public/build/post.js
